24bbf1c9607e3b18df1f5f9b7ef2fffe3c2d8154
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-02-03 00:16:07 +0100

    Version management

--- iscore_addon_csp.cpp
+++ iscore_addon_csp.cpp
@@ -10,6 +10,11 @@ iscore_addon_csp::iscore_addon_csp() :
 {
 }
 
+iscore_addon_csp::~iscore_addon_csp()
+{
+
+}
+
 iscore::GUIApplicationContextPlugin* iscore_addon_csp::make_applicationPlugin(
         const iscore::ApplicationContext& app)
 {
@@ -29,4 +34,14 @@ iscore_addon_csp::factories(
             CSP::MoveEventCSPFlexFactory>
       >
     >(ctx, key);
+}
+
+int32_t iscore_addon_csp::version() const
+{
+    return 1;
+}
+
+UuidKey<iscore::Plugin> iscore_addon_csp::key() const
+{
+    return "8953366a-947c-453c-9e8e-395e0e39be25";
 }
--- iscore_addon_csp.hpp
+++ iscore_addon_csp.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <QObject>
+#include <iscore/plugins/qt_interfaces/PluginRequirements_QtInterface.hpp>
 #include <iscore/plugins/qt_interfaces/GUIApplicationContextPlugin_QtInterface.hpp>
 #include <iscore/plugins/qt_interfaces/FactoryInterface_QtInterface.hpp>
 
@@ -8,25 +9,31 @@
  * In this plugin, we prefer the name time relation for what other call constraints.
  */
 class iscore_addon_csp:
-    public QObject,
-    public iscore::GUIApplicationContextPlugin_QtInterface,
-    public iscore::FactoryInterface_QtInterface
+        public QObject,
+        public iscore::Plugin_QtInterface,
+        public iscore::GUIApplicationContextPlugin_QtInterface,
+        public iscore::FactoryInterface_QtInterface
 {
         Q_OBJECT
         Q_PLUGIN_METADATA(IID GUIApplicationContextPlugin_QtInterface_iid)
         Q_INTERFACES(
-            iscore::GUIApplicationContextPlugin_QtInterface
-            iscore::FactoryInterface_QtInterface
+                iscore::Plugin_QtInterface
+                iscore::GUIApplicationContextPlugin_QtInterface
+                iscore::FactoryInterface_QtInterface
         )
 
     public:
         iscore_addon_csp();
-        virtual ~iscore_addon_csp() = default;
+        virtual ~iscore_addon_csp();
 
+    private:
         iscore::GUIApplicationContextPlugin* make_applicationPlugin(
                 const iscore::ApplicationContext& app) override;
 
         std::vector<std::unique_ptr<iscore::FactoryInterfaceBase>> factories(
                 const iscore::ApplicationContext&,
                 const iscore::AbstractFactoryKey& factoryName) const override;
+
+        int32_t version() const override;
+        UuidKey<iscore::Plugin> key() const override;
 };
